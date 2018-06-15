14ea1eef637d8b7657be5011b4216bf328a0ce6c
neustupov <neustupov@yandex.ru>
2018-06-15 20:33:33 +0300

    added repositories for user and priceRequest with main queries, added spring-data-jpa dependency in pom file

---
+++ src/main/java/ru/neustupov/ordersinthestore/repository/datajpa/DataJpaPriceRequestRepositoryImpl.java
@@ -0,0 +1,34 @@
+package ru.neustupov.ordersinthestore.repository.datajpa;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import ru.neustupov.ordersinthestore.model.PriceRequest;
+import ru.neustupov.ordersinthestore.repository.PriceRequestRepository;
+
+import java.util.List;
+
+@Repository
+public class DataJpaPriceRequestRepositoryImpl implements PriceRequestRepository{
+    @Autowired
+    private CrudPriceRequestRepository crudPriceRequestRepository;
+
+    @Override
+    public PriceRequest save(PriceRequest priceRequest) {
+        return crudPriceRequestRepository.save(priceRequest);
+    }
+
+    @Override
+    public PriceRequest get(int id) {
+        return crudPriceRequestRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public List<PriceRequest> getAll() {
+        return crudPriceRequestRepository.findAll();
+    }
+
+    @Override
+    public boolean delete(int id) {
+        return crudPriceRequestRepository.delete(id) != 0;
+    }
+}
