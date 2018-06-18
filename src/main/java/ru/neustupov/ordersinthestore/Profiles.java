4f5b69b8e15baebb4afa50863608b171dbba61a9
neustupov <neustupov@yandex.ru>
2018-06-18 21:26:10 +0300

    added transfer objects for user, added exceptions, mapper for json, class for profiles, properties for postgres DB, web xml file for dispatching requests, user test data and tests for user service and added all needed dependencies

---
+++ src/main/java/ru/neustupov/ordersinthestore/Profiles.java
@@ -0,0 +1,7 @@
+package ru.neustupov.ordersinthestore;
+
+public class Profiles {
+    public static final String
+            POSTGRES = "postgres",
+            HEROKU = "heroku";
+}
---
+++ src/main/java/ru/neustupov/ordersinthestore/to/UserTo.java
@@ -0,0 +1,67 @@
+package ru.neustupov.ordersinthestore.to;
+
+import org.hibernate.validator.constraints.SafeHtml;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+
+public class UserTo extends BaseTo implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @NotBlank
+    @SafeHtml
+    private String name;
+
+    @Email
+    @NotBlank
+    @SafeHtml
+    private String email;
+
+    @Size(min = 5, max = 32, message = "length must between 5 and 32 characters")
+    private String password;
+
+    public UserTo() {
+    }
+
+    public UserTo(Integer id, String name, String email, String password) {
+        super(id);
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "UserTo{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
